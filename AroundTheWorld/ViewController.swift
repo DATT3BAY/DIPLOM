49ddfb587eacc99fa9941b39e74cc09860e46f78
DATT3BAY <anisimovaleksandr23@outlook.com>
2022-03-11 15:48:29 +0700

    Добавление бэкграунда

--- AroundTheWorld/ViewController.swift
+++ AroundTheWorld/ViewController.swift
@@ -7,19 +7,34 @@
 //
 
 import UIKit
-
+import AVFoundation
 class ViewController: UIViewController {
 
     @IBOutlet weak var gifImage: UIImageView!
+    @IBOutlet weak var videoLayer: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        
         gifImage.loadGif(name: "gifka_zemli2")
-        
-        
-        // Do any additional setup after loading the view.
+        playVideo()
     }
 
+    func playVideo() {
+        guard let path = Bundle.main.path(forResource: "intro", ofType: "mp4")
+            else {
+                return
+        }
+        let player = AVPlayer(url: URL(fileURLWithPath: path))
+        let playerLayer = AVPlayerLayer(player: player)
+        playerLayer.frame = self.view.bounds
+        playerLayer.videoGravity = .resizeAspectFill
+        self.videoLayer.layer.addSublayer(playerLayer)
+        player.play()
+        
+        videoLayer.bringSubviewToFront(gifImage)
+    }
 
 }
 
