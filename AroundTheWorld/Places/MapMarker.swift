99e06654573890f7d468b0bdd948c2717a27ace1
DATT3BAY <anisimovaleksandr23@outlook.com>
2022-04-07 16:06:58 +0700

    Добавление карты с маркером места

---
+++ AroundTheWorld/Places/MapMarker.swift
@@ -0,0 +1,43 @@
+//
+//  MapMarker.swift
+//  AroundTheWorld
+//
+//  Created by Александр Анисимов on 30.03.2022.
+//  Copyright © 2022 Александр Анисимов. All rights reserved.
+//
+
+import SwiftUI
+
+struct MapMarker: View {
+    @State var animate = false
+    let placeName: String
+    var body: some View {
+        ZStack {
+            Circle()
+                .fill(Color.red)
+                .frame(width: 10, height: 10)
+            
+            Circle()
+                .fill(Color.red.opacity(0.2))
+                .frame(width: 10, height: 10)
+                .scaleEffect(animate ? 8.0 : 1.0)
+                .opacity(animate ? 0.1 : 1.0)
+                .animation(Animation.linear(duration: 2.5).repeatForever(autoreverses: false))
+            
+            GeometryReader { geometry in
+                ZStack {
+                    Text(placeName)
+                        .font(.system(size: 14))
+                        .padding(6)
+                        .background(Color.white)
+                        .cornerRadius(5)
+                        .fixedSize()
+                }
+                .offset(x: geometry.size.width/2 + 15, y: -8)
+            }
+        }
+        .onAppear {
+            animate = true
+        }
+    }
+}
