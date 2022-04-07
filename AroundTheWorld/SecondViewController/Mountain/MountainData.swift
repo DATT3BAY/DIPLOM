550f33a7a9ff5cb964a6ee4652011744935d188d
DATT3BAY <anisimovaleksandr23@outlook.com>
2022-04-07 16:04:48 +0700

    Данные для гор

---
+++ AroundTheWorld/SecondViewController/Mountain/MountainData.swift
@@ -0,0 +1,20 @@
+//
+//  TravelData.swift
+//  AroundTheWorld
+//
+//  Created by Александр Анисимов on 03.04.2022.
+//  Copyright © 2022 Александр Анисимов. All rights reserved.
+//
+
+import SwiftUI
+
+struct MountainData: Identifiable{
+    var id: Int
+    var imageMount: String
+    var cntry: String
+    var plce: String
+    var detailss: String
+    
+}
+
+
