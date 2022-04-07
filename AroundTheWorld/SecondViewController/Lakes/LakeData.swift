f6f06f952d16c8210700daa208a267b8984ced94
DATT3BAY <anisimovaleksandr23@outlook.com>
2022-04-07 16:04:03 +0700

    Данные для озер

---
+++ AroundTheWorld/SecondViewController/Lakes/LakeData.swift
@@ -0,0 +1,18 @@
+//
+//  LakeData.swift
+//  AroundTheWorld
+//
+//  Created by Александр Анисимов on 06.04.2022.
+//  Copyright © 2022 Александр Анисимов. All rights reserved.
+//
+
+import SwiftUI
+
+
+struct LakeData: Identifiable{
+    var id: Int
+    var imageLake: String
+    var cntry2: String
+    var plce2: String
+    var detailss2: String
+}
